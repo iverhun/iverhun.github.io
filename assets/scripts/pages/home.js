7f9a77c7baf1877319e357e21d091d4a3ca5afef
Ivan Verhun <902581+iverhun@users.noreply.github.com>
2024-04-17 13:04:36 +0300

    fixed a few URLs and other mistakes

---
+++ assets/scripts/pages/home.js
@@ -0,0 +1,19 @@
+import { init } from 'ityped'
+
+// =========== Typing Carousel ================
+// get data from hidden ul and set as typing data
+document.addEventListener('DOMContentLoaded', () => {
+  const $ul = document.getElementById('typing-carousel-data')?.children
+  if ($ul == null || $ul.length === 0) return
+
+  const strings = Array.from($ul).map($el => $el.textContent)
+
+  init('#ityped', {
+    strings,
+    startDelay: 400,
+    backDelay:  900,
+    typeSpeed:  80,
+    loop: true,
+    disableBackTyping: false
+  })
+})
